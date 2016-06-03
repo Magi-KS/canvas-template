791bd3537a95f06effb69b11345de28448b0f809
Crystal Toh <crystal.hueyjing@mindvalley.com>
2016-06-03 12:15:47 +0800

    snake

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ var DOWN = 40
 // game variables
 var dot_size = 10;
 var initial_pos = {x:5, y:5}
+var map_size = {x: canvas.width/dot_size, y: canvas.height/dot_size}
 // game state variables
-// var food = {x:}
+var food = randomPos()
 var snake_direction = RIGHT;
 var snake = [
   {x:initial_pos.x, y:initial_pos.y},
@@ -33,6 +34,14 @@ document.addEventListener('keydown',function(e){
   }
 });
 
+function randomPos(){
+  return{
+    x: Math.floor((Math.random() * map_size.x)),
+    y: Math.floor((Math.random() * map_size.y))
+  }
+
+}
+
 function sMoveUpdate(){
   var new_head = {
     x: snake[0].x,
@@ -44,28 +53,50 @@ function sMoveUpdate(){
     case RIGHT: new_head.x += 1; break;
     case DOWN: new_head.y += 1; break;
   }
-  snake.push(new_head);
-  // console.log(new_head);
+
+  if (new_head.x >= map_size.x){
+    new_head.x = 0;
+  }
+  else if (new_head.x < 0) {
+    new_head.x = map_size.x;
+  }
+
+  if (new_head.y >= map_size.y){
+    new_head.y = 0;
+  }
+  else if (new_head.y < 0) {
+    new_head.y = map_size.y;
+  }
+
+  snake.reverse().push(new_head);
   snake.reverse().pop();
 }
 
 function draw(){
   context.clearRect(0, 0, 300, 300);
-  for(var i = snake.length; i > 0 ; i--){
-    var x_pos = snake[i-1].x * dot_size;
-    var y_pos = snake[i-1].y * dot_size;
+  for(var i = 0; i < snake.length ; i++){
+    var x_pos = snake[i].x * dot_size;
+    var y_pos = snake[i].y * dot_size;
     context.fillRect(x_pos, y_pos, dot_size, dot_size);
   }
+  context.fillRect(food.x * dot_size, food.y * dot_size, dot_size, dot_size);
 }
-function gameUpdate(){
-
+function foodUpdate(){
+  if((snake[0].x == food.x) && (snake[0].y == food.y)){
+    snake.push({
+      x: snake[snake.length-1].x,
+      y: snake[snake.length-1].y
+    });
+    food = randomPos();
+  }
 }
 
 var last_loop = 0;
 function gameLoop(time){
-  if((time - last_loop) >= 16.666666667){
+  if((time - last_loop) >= 16.666666667*4){
     last_loop = time
     sMoveUpdate();
+    foodUpdate();
     draw();
   }
   requestAnimationFrame(gameLoop);
